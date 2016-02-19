77982f90ad7b8db8df1e3f5ac5e94d269d05a7e7
simonedesordi <simone.desordi@gmail.com>
2016-02-19 17:53:06 +0100

    running

--- static/client.js
+++ static/client.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
 
+    require(["LightstreamerClient", "Subscription", "StaticGrid"], function (LightstreamerClient, Subscription, StaticGrid) {
+
+        var client = new LightstreamerClient('http://ec2-54-191-14-197.us-west-2.compute.amazonaws.com:8080', 'HELLOWORLD');
+        client.connect();
+
+        var grid = new StaticGrid("hellogrid", true);
+
+        var subscription = new Subscription("MERGE", grid.extractItemList(), grid.extractFieldList());
+        subscription.addListener(grid);
+
+        client.subscribe(subscription);
+    });
+
 });
