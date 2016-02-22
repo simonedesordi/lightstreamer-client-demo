719622941d4da85667f327a99bc5a0ee500b0ad5
simonedesordi <simone.desordi@gmail.com>
2016-02-22 10:58:30 +0100

    adding new subscription

--- static/client.js
+++ static/client.js
@@ -13,4 +13,30 @@ $(document).ready(function () {
         client.subscribe(subscription);
     });
 
+    require(["js/lsClient", "Subscription", "DynaGrid"], function (lsClient, Subscription, DynaGrid) {
+
+        portfolioGrid = new DynaGrid("portfolio", true);
+        portfolioGrid.setAutoCleanBehavior(true, false);
+        portfolioGrid.addListener({
+            onVisualUpdate: function (key, info) {
+                if (info == null) {
+                    return;
+                }
+                // visual effects on updates
+                info.setHotTime(hotTime);
+                info.setStyle("lshot", "lscold");
+                info.setCellStyle("command", "commandhot", "commandcold")
+            }
+        });
+        // let's define the initial sorting column
+        changeSort(initialSort);
+
+        var portfolioSubscription = new Subscription("COMMAND", portfolioId, fieldList);
+        portfolioSubscription.setDataAdapter("PORTFOLIO_ADAPTER");
+        portfolioSubscription.setRequestedSnapshot("yes");
+
+        portfolioSubscription.addListener(portfolioGrid);
+        lsClient.subscribe(portfolioSubscription);
+    });
+
 });
